cf19884d6a4b53234d4b50a4171ca0b8bee91ddc
Gary Kertopermono <garycxjk@gmail.com>
2019-05-13 23:38:19 +0200

    The createTile method promise will resolve with the path and colors

--- src/renderer/helpers/MosaicTileManager.js
+++ src/renderer/helpers/MosaicTileManager.js
@@ -7,18 +7,32 @@ import { format as formatUrl } from 'url';
 const validEncoding = {
   'image/png': 'png',
   'image/jpeg': 'jpg',
+  'image/webp': 'webp',
 };
 
-class MosaicTileManager {
-  static createTile(options) {
-    const {
-      file,
-      width,
-      height,
-      mode = 'cover',
-      encoding = 'image/png',
-    } = options;
+/**
+ * @typeof {Object} TileOptions The options for creating a tile.
+ * @property {String} file The file to generate the tile from.
+ * @property {number} width The width of the tile.
+ * @property {number} height The height of the tile.
+ */
 
+/**
+ * This helper class will help in creating and analyzing tiles.
+ */
+class MosaicTileManager {
+  /**
+   * Creates a tile from a select image.
+   * @param {TileOptions} options The {@link TileOptions}.
+   */
+  static createTile({
+    file,
+    width,
+    height,
+    mode = 'cover',
+    encoding = 'image/png',
+  }) {
+    // Check if the provided encoding is valid.
     if (!validEncoding[encoding]) {
       return Promise.reject();
     }
@@ -78,15 +92,36 @@ class MosaicTileManager {
           if (err) {
             reject(err);
           }
-          resolve(imgOut);
+          const colors = this.getColors(image);
+          resolve({
+            thumb: imgOut,
+            colors,
+          });
         });
       };
     });
   }
 
-  static getColors() {
-    const canvas = document.createElement('canvas');
-    canvas.getContext('2d');
+  static getColors(canvas, x = 0, y = 0, width = 0, height = 0) {
+    const testCanvas = document.createElement('canvas');
+    testCanvas.width = 1;
+    testCanvas.height = 1;
+    const context = testCanvas.getContext('2d');
+    const realWidth = (width || canvas.width) - x;
+    const realHeight = (height || canvas.height) - y;
+    const colors = [];
+    for (let posY = 0; posY < 2; posY += 1) {
+      for (let posX = 0; posX < 2; posX += 1) {
+        const sourceX = Math.floor(posX * (realWidth / 2));
+        const sourceY = Math.floor(posY * (realHeight / 2));
+        const sourceWidth = Math.floor((posX + 1) * (realWidth / 2)) - sourceX;
+        const sourceHeight = Math.floor((posY + 1) * (realWidth / 2)) - sourceY;
+        context.drawImage(canvas, sourceX + x, sourceY + y, sourceWidth, sourceHeight, 0, 0, 1, 1);
+        const color = context.getImageData(0, 0, 1, 1).data;
+        colors.push(color.slice(0, 3));
+      }
+    }
+    return colors;
   }
 }
 
