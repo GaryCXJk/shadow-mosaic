b0e08a78a8a808810e0c640abffcfbf6eba7c336
Gary Kertopermono <gary-kertopermono@prems.work>
2019-05-17 18:07:13 +0200

    Expanded the tileset reducer

--- src/renderer/store/reducers/tilesets.js
+++ src/renderer/store/reducers/tilesets.js
@@ -5,16 +5,59 @@ import {
   TILESETS_REMOVE_TILE,
 } from '@store/actions/tilesets';
 
+const merge = require('deepmerge');
+
+const tileset = (action, state = {
+  name: '',
+  file: null,
+  tiles: [],
+}) => {
+  switch (action.type) {
+    case TILESETS_ADD_TILE:
+      return {
+        ...state,
+        tiles: [
+          ...merge([], state.tiles),
+          action.tile,
+        ],
+      };
+    case TILESETS_ORDER_TILE: {
+      const tiles = merge([], state.tiles);
+      const tile = tiles.splice(action.oldPosition, 1);
+      tiles.splice(action.newPosition, 0, tile);
+      return {
+        ...state,
+        tiles,
+      };
+    }
+    case TILESETS_REMOVE_TILE: {
+      const tiles = merge([], state.tiles);
+      tiles.splice(action.position, 1);
+      return {
+        ...state,
+        tiles,
+      };
+    }
+    case TILESETS_CREATE:
+    default:
+      return state;
+  }
+};
+
 const tilesetReducer = (state = [], action) => {
   switch (action.type) {
     case TILESETS_CREATE:
-      return state;
+      return [
+        ...merge([], state),
+        tileset(action),
+      ];
     case TILESETS_ADD_TILE:
-      return state;
     case TILESETS_ORDER_TILE:
-      return state;
-    case TILESETS_REMOVE_TILE:
-      return state;
+    case TILESETS_REMOVE_TILE: {
+      const tilesets = merge([], state);
+      tilesets[action.tileset] = tileset(action, state[action.tileset]);
+      return tilesets;
+    }
     default:
       return state;
   }
